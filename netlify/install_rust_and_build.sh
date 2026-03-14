f8065f589ab0cd3656d1ffa097501894bd6ec68b
atharva2003 <atharvajawalka20@gmail.com>
2026-03-14 18:35:45 +0530

    Move requirements.txt to prevent Netlify auto-installing before Rust is available

--- netlify/install_rust_and_build.sh
+++ netlify/install_rust_and_build.sh
@@ -6,12 +6,13 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Load cargo environment for the rest of the build
 source "$HOME/.cargo/env"
+export PATH="$HOME/.cargo/bin:$PATH"
 
 # Ensure pip is up-to-date
 pip install --upgrade pip
 
-# Now continue with the normal dependency install
-pip install -r requirements.txt
+# Now continue with the normal dependency install from moved file
+pip install -r frontend/requirements.txt
 
 # Netlify normally handles the rest of the build or serving of static files,
 # but since the backend is python (FastAPI), Netlify can only serve the `frontend` folder statically.
